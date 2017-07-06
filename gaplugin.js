576cb0b7183986f2a61caebd6c572c53149858e0
Dmytro Bulakh <postman31@gmail.com>
2017-07-06 23:54:59 +0300

    Update gaplugin.js

--- gaplugin.js
+++ gaplugin.js
@@ -1,4 +1,11 @@
 (function() {
+  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+    n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+    document,'script','https://connect.facebook.net/en_US/fbevents.js');
+  var fbq = window.fbq;
+  fbq('init', '379482319092836');
 
   // Assign the ga variable to the Google Analytics global function
   var ga = window[window['GoogleAnalyticsObject'] || 'ga'];
@@ -24,6 +31,7 @@
       //var xhr = new XMLHttpRequest();
       //xhr.open('POST', 'http://process.simoahava.com/collect', true);
       //xhr.send(model.get('hitPayload'));
+      fbq('trackCustom', 'MyCustomEvent', {custom_param: 'custom_value'});
       console.log(model.get('hitPayload'));
     });
   };
